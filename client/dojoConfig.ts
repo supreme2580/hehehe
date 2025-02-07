5eec2cbbe51e673e52e8fa49a7d5d4b671921d89
Supreme2580 <victoromorogbe69@gmail.com>
2025-02-07 19:00:27 +0100

    gfhfh

---
+++ client/dojoConfig.ts
@@ -0,0 +1,10 @@
+import { createDojoConfig } from "@dojoengine/core";
+
+import manifest from "../../worlds/dojo-starter/manifest_dev.json";
+
+export const dojoConfig = createDojoConfig({
+    manifest,
+});
+
+
+
